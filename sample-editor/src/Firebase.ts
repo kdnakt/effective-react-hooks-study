094c407ccbcca57bcefbaba506a66953a303f402
kdnakt <a.kid.1985@gmail.com>
2019-08-08 09:45:10 +0900

    add Firebase.ts

---
+++ sample-editor/src/Firebase.ts
@@ -0,0 +1,23 @@
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+interface FirebaseContext {
+  userId: string | null,
+  userName: string,
+}
+
+export const FirebaseContext = React.createContext<FirebaseContext>({
+  userId: null,
+  userName: '',
+});
