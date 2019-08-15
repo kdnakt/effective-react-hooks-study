fccd55733d19d64bdf6615ac843903a236279e7b
kdnakt <a.kid.1985@gmail.com>
2019-08-15 10:45:56 +0900

    rewrite App.tsx

--- sample-editor/src/App.tsx
+++ sample-editor/src/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { History } from 'history';
 
-const App: React.FC = () => {
-  return <div>hoge</div>
+import { FirebaseAuth, signInWithRedirect, signOut } from './FirebaseAuth';
+
+import { Router } from './Router';
+import { sign } from 'crypto';
+
+const Content: React.FC<{ history : History }> = ({ history }) => {
+  return <Router history={history} />;
+};
+
+const App: React.FC<{ history: History }> = ({ history }) => {
+  const NotSignedIn = React.useCallback(() => {
+    return <button onClick={() => signInWithRedirect()}>Sign In</button>;
+  }, []);
+  const Loading = React.useCallback(() => {
+    return <div>Loading Now ....</div>;
+  }, []);
+
+  return (
+    <FirebaseAuth NotSignedIn={NotSignedIn} Loading={Loading}>
+      <Content history={history} />
+      <button onClick={signOut}>Sign Out</button>
+    </FirebaseAuth>
+  );
 }
 
 export default App;
