1a3220439d0ffe2ad30505a19e4540512c1c7d64
kdnakt <a.kid.1985@gmail.com>
2019-08-20 19:59:08 +0900

    remove logo and css

--- sandbox-editor/src/App.tsx
+++ sandbox-editor/src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const AppDiv = styled.div`
+  width: 100vw;
+  height: 100vw;
+  overflow: hidden;
+  margin: 0px;
+  padding: 0px;
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppDiv>
+      hello
+    </AppDiv>
   );
 }
 
