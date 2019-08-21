36a939f26668e334855a89076b74528f9648c8c7
kdnakt <a.kid.1985@gmail.com>
2019-08-22 01:43:42 +0900

    create monaco editor instance

--- sandbox-editor/src/components/SandboxHooks.ts
+++ sandbox-editor/src/components/SandboxHooks.ts
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
+
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 export const useSandbox = () => {
   const editorDiv = React.useRef<HTMLDivElement>(null);
+  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor>();
+  const subscriptionRef = useRef<monaco.IDisposable[]>([]);
+
+  const unsubscribe = () => {
+    subscriptionRef.current.forEach(subscription => {
+      subscription.dispose();
+    });
+    subscriptionRef.current = [];
+  };
+
+  useLayoutEffect(() => {
+    console.log('useLayoutEffect', editorDiv.current);
+    const height = editorDiv.current!.parentElement!.clientHeight
+        - editorDiv.current!.offsetTop;
+    editorDiv.current!.style.height = `${height}px`;
+
+    editorRef.current = monaco.editor.create(editorDiv.current!, {
+      minimap: {
+        enabled: true,
+      },
+      fontSize: 16,
+      lineNumbers: 'on',
+      wordWrap: 'on',
+      automaticLayout: true,
+    });
+    editorRef.current.focus();
+
+    return () => {
+      console.log('useLayoutEffect disposed');
+      if (editorRef.current) {
+        editorRef.current.dispose();
+        editorRef.current = undefined;
+      }
+      unsubscribe();
+    };
+  }, []);
 
   return { editorDiv };
 };
