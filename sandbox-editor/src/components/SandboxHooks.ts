4ca9a9a45310aa7d7ae7a610b7ba7c0ffed780f7
kdnakt <a.kid.1985@gmail.com>
2019-08-24 09:49:49 +0900

    register document formatting edit provider

--- sandbox-editor/src/components/SandboxHooks.ts
+++ sandbox-editor/src/components/SandboxHooks.ts
@@ -2,6 +2,24 @@ import React, { useLayoutEffect, useRef } from 'react';
 
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
+monaco.languages.registerDocumentFormattingEditProvider('javascript', {
+  async provideDocumentFormattingEdits(model) {
+    const prettier = await import('prettier/standalone');
+    const babylon = await import('prettier/parser-babylon');
+    const text = prettier.format(model.getValue(), {
+      parser: 'babel',
+      plugins: [babylon],
+      singleQuote: true,
+      tabWidth: 2,
+    });
+    console.log('format');
+
+    return [
+      { range: model.getFullModelRange(), text }
+    ];
+  }
+});
+
 export const useSandbox = () => {
   const editorDiv = React.useRef<HTMLDivElement>(null);
   const editorRef = useRef<monaco.editor.IStandaloneCodeEditor>();
