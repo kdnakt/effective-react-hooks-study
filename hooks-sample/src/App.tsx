c33bb235f2e8ec7d67a394a2abbf2ab87be6c3fd
kdnakt <a.kid.1985@gmail.com>
2019-09-02 23:11:02 +0900

    useEffect()

--- hooks-sample/src/App.tsx
+++ hooks-sample/src/App.tsx
@@ -1,9 +1,19 @@
 import React, {
-  useState,
+  useState, useEffect,
 } from 'react';
 
 const App: React.FC = () => {
   const [name, setName] = useState('');
+
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCounter(prev => prev + 1);
+    }, 500);
+    return () => {
+      clearInterval(id);
+    }
+  }, [counter]);
 
   return (
     <div>
@@ -12,6 +22,8 @@ const App: React.FC = () => {
       <input value={name} name="myname" onChange={(e) => setName(e.target.value)} />
       <hr />
       <h5>Your name is {name}</h5>
+      <hr />
+      <h5>{counter}</h5>
     </div>
   );
 }
